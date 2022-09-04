7cdfd12d0ab2fd6d1bf27387b454750f5bfc3470
faazizpro <fahadafs11@gmail.com>
2022-09-04 10:18:20 +0600

    Added author image and name

--- js/news.js
+++ js/news.js
@@ -57,33 +57,40 @@ const displayMenu = menus => {
                            <img class="lg:h-72 md:h-48 w-full object-cover object-center"
                                src="${item.image_url}" alt="blog">
                            <div class="p-6">
-                               <h2 class="text-base font-medium text-indigo-400 mb-1">4 September, 2022</h2>
+                               <h2 class="text-base font-medium text-indigo-400 mb-1"></h2>
                                <h1 class="text-2xl font-semibold mb-3">${item.title}</h1>
                                <p class="leading-relaxed mb-3">${item.details}</p>
-                               <div class="flex items-center flex-wrap ">
-                                   <a class="text-indigo-400 inline-flex items-center md:mb-2 lg:mb-0">Read More
-                                       <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"
-                                           fill="none" stroke-linecap="round" stroke-linejoin="round">
-                                           <path d="M5 12h14"></path>
-                                           <path d="M12 5l7 7-7 7"></path>
-                                       </svg>
-                                   </a>
-                                   <span
+
+                               
+                               
+                               <div class="flex items-center justify-center ">
+                                    <a class="text-indigo-500 font-bold inline-flex items-center md:mb-2 lg:mb-0">Read More
+                                        <svg 
+                                            class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"
+                                            fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                            <path d="M5 12h14"></path>
+                                            <path d="M12 5l7 7-7 7"></path>
+                                        </svg>
+                                    </a>
+                               
+                                    <span 
+                                        class="text-gray-400 mr-3 inline-flex items-center text-sm pr-3 py-1      border-gray-200">
+                                        <div class="inline-flex items-center justify-center flex-wrap">
+                                            <img src="${item.author.img}" class="rounded w-10 ml-10 mr-3">
+                                            <div>
+                                                <p>${item.author.name}</p>
+                                            </div>
+                                        </div>
+                                    </span> 
+
+                                    <span
                                        class="text-gray-400 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1  border-gray-200">
                                        <svg class="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none"
                                            stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
                                            <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
                                            <circle cx="12" cy="12" r="3"></circle>
                                        </svg>${item.total_view}
-                                   </span>
-                                   <!-- <span class="text-gray-400 inline-flex items-center leading-none text-sm">
-                                       <svg class="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none"
-                                           stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
-                                           <path
-                                               d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z">
-                                           </path>
-                                       </svg>6
-                                   </span> -->
+                                    </span>
                                </div>
                            </div>
                        </div>
